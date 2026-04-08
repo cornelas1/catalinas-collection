bf815a9ffc455143c2d5b0f7aa95f6615faca1c0
cornelas1 <cesar@oinjurylaw.com>
2026-04-08 12:30:03 -0500

    Update sw.js cache version to v3
    Updated service worker to use a new cache version and added asset URLs for caching.

--- sw.js
+++ sw.js
@@ -1,3 +1,33 @@
+const CACHE_NAME = 'catalinas-collection-v3';
+const ASSETS = [
+  '/',
+  '/index.html',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
 const CACHE_NAME = 'catalinas-collection-v2';
 const ASSETS = [
   '/',
